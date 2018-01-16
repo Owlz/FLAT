fe8cb6ee5595cb6b47fcd799d0db5cd9e8c63184
Fravit <Fravit@172.19.46.108>
2018-01-16 10:44:33 +0100

    Solv problem

--- __progettoIS/src/applicationLogic/exception/DatiOccupati.java
+++ __progettoIS/src/applicationLogic/exception/DatiOccupati.java
@@ -2,25 +2,8 @@ package applicationLogic.exception;
 
 public class DatiOccupati extends Exception {
 	private static final long serialVersionUID = 1L;
-	private String campo = null;
 
 	public DatiOccupati() {
 		super("Dati già in uso");
 	}
-
-	public DatiOccupati(String message) {
-		super("Dati già in uso: " + ottieniCampo(message));
-		this.campo = ottieniCampo(message);
-		/* TODO: qui le cose si ripetono due volte, è brutto ma è l'unico modo */
-	}
-
-	private static String ottieniCampo(String str) {
-		String msg = str.split("'")[3].trim();
-		if(msg.equals("PRIMARY_KEY")) msg = "nickname";
-		if(msg.equals("email_UNIQUE")) msg = "email";
-		return msg;
-	}
-
-	public String getCampo() { return campo; }
-	public void setCampo(String campo) { this.campo = campo; }
 }
