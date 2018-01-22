cdaee2aec19b6386a906007207dd254d36deb9af
Luca <urban97-luca@hotmail.it>
2018-01-22 11:37:48 +0100

    Aggiunto filtro per evitare di accedere alle view

---
+++ __progettoIS/src/applicationLogic/controller/FilterNoJSP.java
@@ -0,0 +1,24 @@
+package applicationLogic.controller;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter per bloccare l'accesso a pagine .jsp (che devono gestire la sola view)
+ */
+@WebFilter("*.jsp")
+public class FilterNoJSP implements Filter {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletResponse res = (HttpServletResponse) response;
+		HttpServletRequest req = (HttpServletRequest) request;
+		res.sendRedirect(req.getContextPath());
+	}
+}
